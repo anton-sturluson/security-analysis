fc3f6728d899a07816d02320bc48c2c9ef293c45
Anton Sturluson <chyoo@othot.com>
2020-08-27 19:01:48 -0400

    more utility functions added. variable names changed.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,18 @@
-import os
+from os import path
 
+# path to crawler dir
+CRAWLERDIR = "crawler"
 # path to log
-LOGPATH = os.path.join("crawler", "log.txt")
+LOGPATH = path.join(CRAWLERDIR, "log.txt")
 # path to result
-RESULTPATH = os.path.join("crawler", "result.json")
+RESULTPATH = path.join(CRAWLERDIR, "result.json")
+# path to mapping.json
+MAPPINGPATH = path.join(CRAWLERDIR, "mapping.json")
 # path to data directory
-DATADIR = os.path.join("data")
+DATADIR = path.join("data")
 # path to company directory
-COMPANYDIR = os.path.join(DATADIR, "company")
+COMPANYDIR = path.join(DATADIR, "company")
 # path to profile
-PROFILEPATH = os.path.join(DATADIR, "stock_profile.csv")
+PROFILEPATH = path.join(DATADIR, "stock_profile.csv")
 # path to backup profile
-PROFILEBACKPATH = os.path.join(DATADIR, "stock_profile_backup.csv")
+PROFILEBACKPATH = path.join(DATADIR, "stock_profile_backup.csv")
--- utils/tools.py
+++ utils/tools.py
@@ -1,24 +1,37 @@
 import csv
 import sys
 import json
+from datetime import datetime, timedelta
 from functools import lru_cache
 from os import path, rename, system
 
 import numpy as np
 import pandas as pd
 
-from utils import PROFILEPATH, COMPANYDIR, LOGPATH, RESULTPATH
-from crawler.mapping import month2digit, var2filename, var2filetype
+from utils import PROFILEPATH, COMPANYDIR, LOGPATH, RESULTPATH, MAPPINGPATH
+
+
+def get_mapping():
+    with open(MAPPINGPATH, "r") as r_obj:
+        return json.load(r_obj)
+
+mapping = get_mapping()
+col2filename = mapping["col2filename"]
+col2dtype = mapping["col2dtype"]
 
 
 def cp(from_, to_):
     if path.exists(from_):
+        # check if destination dir exists
+        tmp = "/".join((tmp := to_.split("/"))[0:len(tmp)-1])
+        if not path.exists(tmp):
+            mkdir(tmp)
         system(f"cp {from_} {to_}")
 
 
 def mv(from_, to_):
     if path.exists(from_):
-        # check if path to the file exists
+        # check if destination dir exists
         tmp = "/".join((tmp := to_.split("/"))[0:len(tmp)-1])
         if not path.exists(tmp):
             mkdir(tmp)
@@ -34,22 +47,30 @@ def rm(path):
         system(f"rm {path}")
 
 
-def log(msg, is_debug=False, is_print=True):
-    if not is_debug:
+def to_csv(df, outpath, index=True):
+    n = len(splitted := outpath.split("/"))
+    if not path.exists(outdir := path.join(*splitted[:n - 1])):
+        mkdir(outdir)
+    df.to_csv(outpath, index=index)
+
+
+
+def log(msg, debug=False, verbose=True):
+    if not debug:
         with open(LOGPATH, "a") as w_obj:
             w_obj.write(f"{msg.strip()}\n")
 
-    if is_print:
+    if verbose:
         print(msg)
 
 
-def json_dump(json_obj, is_debug=False, is_print=True, indent=4):
-    if not is_debug:
+def json_dump(json_obj, debug=False, verbose=True, indent=4):
+    if not debug:
         with open(RESULTPATH, "w") as w_obj:
             json.dump(json_obj, w_obj, indent=indent)
             w_obj.write("\n")
 
-    if is_print:
+    if verbose:
         print(json.dumps(json_obj, indent=indent))
 
 
@@ -60,58 +81,86 @@ def get_symbols(stock_only=False):
     return df["Symbol"].values
 
 
+def get_today():
+    """Get the date at the point where data is crawled.
+
+        If it is between 12am and 8am, return yeterday's date."""
+    today = datetime.today()
+    premkt = today.replace(hour=8, minute=0, second=0, microsecond=0)
+    if today < premkt:
+        return (today - timedelta(days=1)).date()
+    return today.date()
+
+
 def path2dir(path):
     return "/".join((tmp := path.split("/"))[:len(tmp)-1])
 
 
 @lru_cache()
-def get_path(var, symbol, is_yearly=False, is_backup=False, is_debug=False):
-    if var2filename[var] == "profile":
+def get_path(col, symbol, yearly=False, backup=False, debug=False, original=False):
+    if col2filename[col] == "profile":
         return PROFILEPATH
 
-    if symbol is None:
+    if not symbol:
         raise Exception("ERROR get_path(): symbol shouldn't be None if filename != 'profile'")
 
-    t = "yearly" if is_yearly else "quarterly"
-    file_ = (path_append(symbol, t, var2filename[var])
-             if var2filename[var] in ["income_statement", "balance_sheet", "cash_flow"]
-             else path_append(symbol, var2filename[var]))
+    t = "yearly" if yearly else "quarterly"
+    filename = (name_append(symbol, t, col2filename[col])
+                if col2filename[col] in {"income_statement", "balance_sheet",
+                                         "cash_flow", "statistics"}
+                else name_append(symbol, col2filename[col]))
 
-    child = "backup" if is_backup else "debug"
-    if is_backup or is_debug:
-        return path.join(COMPANYDIR, symbol, child,
-                         path_append(file_, child, filetype="csv"))
+    if backup or debug or original:
+        if backup:
+            subpath = "backup"
+        elif debug:
+            subpath = "debug"
+        elif original:
+            subpath = "original"
+        return path.join(COMPANYDIR, symbol, subpath,
+                         name_append(filename, subpath, filetype="csv"))
     else:
         return path.join(COMPANYDIR, symbol,
-                         path_append(file_, filetype="csv"))
+                         name_append(filename, filetype="csv"))
 
 
-def path_append(*args, sep="_", filetype=None):
+@lru_cache()
+def name_append(*args, sep="_", filetype=None):
     tmp = sep.join(list(args))
-    return (tmp if filetype is None
-            else ".".join([tmp, filetype]))
+    return ".".join([tmp, filetype]) if filetype else tmp
+
+
+@lru_cache()
+def get_df(col, symbol=None, yearly=False, sep=",",
+           index_col=0, convert_index_to_datetime=True, debug=False):
+    if path.exists(inpath := get_path(col, symbol, yearly, debug=debug)):
+        return path2df(inpath, sep, index_col, convert_index_to_datetime)
 
 
 
 @lru_cache()
-def var2df(var, symbol=None, is_yearly=False, sep=",", index_col=0):
-    if path.exists(inpath := get_path(var, symbol, is_yearly)):
-        return path2df(inpath, sep=sep, index_col=index_col)
-
-
-@lru_cache()
-def path2df(path, sep=",", index_col=0):
+def path2df(path, sep=",", index_col=0, convert_index_to_datetime=True):
     df = pd.read_csv(path, sep=sep, index_col=index_col)
-    if index_col is not None:
-        df = df[(~pd.isnull(df.index)) & (df.index != "ttm")]
+    if convert_index_to_datetime:
         df.index = pd.to_datetime(df.index, errors="coerce")
     return df
 
 
+def backup_and_save_df(col, symbol, df, init, debug, index=True):
+    outpath = get_path(col, symbol, debug=debug)
+    if not debug and path.exists(outpath):
+        if init:
+            originalpath = get_path(col, symbol, original=True)
+            cp(outpath, originalpath) # store the original file if init
+        backpath = get_path(col, symbol, backup=True)
+        mv(outpath, backpath)
+    to_csv(df, outpath, index=index)
+
+
 @lru_cache()
-def get_data(var,
+def get_data(col,
              symbol=None,
-             is_yearly=False,
+             yearly=False,
              i_start=None,
              i_end=None,
              sep=","):
@@ -121,16 +170,16 @@ def get_data(var,
         except StopIteration:
             return
 
-    is_profile = True if symbol is None else False
+    profile = True if symbol is None else False
 
-    if not path.exists(inpath := get_path(var, symbol, is_yearly)):
+    if not path.exists(inpath := get_path(col, symbol, yearly)):
         return
 
     if i_start is None and i_end is None: # return whole vector
         i_start = 0
         i_end = sys.maxsize # HMM...
 
-    skip_ttm = (True if var2filename[var] in ["cash_flow", "income_statement"]
+    skip_ttm = (True if col2filename[col] in ["cash_flow", "income_statement"]
                 else False)
 
     with open(inpath, "r") as r_obj:
@@ -153,16 +202,16 @@ def get_data(var,
             tmp = reader2next(reader)
 
             if ri > i_start:
-                if not is_profile and prev["Symbol"] == symbol:
+                if not profile and prev["Symbol"] == symbol:
                     dates.append(prev["Date"])
-                if var2filetype[var] == "datetime":
-                    res.append(pd.to_datetime(prev[var], errors="coerce"))
-                elif var2filetype[var] == "float":
-                    res.append(pd.to_numeric(prev[var], errors="coerce"))
-                elif var2filetype[var] == "bool":
-                    res.append(bool(prev[var]))
+                if col2dtype[col] == "datetime":
+                    res.append(pd.to_datetime(prev[col], errors="coerce"))
+                elif col2dtype[col] == "float":
+                    res.append(pd.to_numeric(prev[col], errors="coerce"))
+                elif col2dtype[col] == "bool":
+                    res.append(bool(prev[col]))
                 else: # string
-                    res.append(prev[var])
+                    res.append(prev[col])
 
                 if return_scala:
                     return res[0]
@@ -172,4 +221,4 @@ def get_data(var,
             ri += 1
 
         if len(res) > 0:
-            return pd.Series(res) if is_profile else pd.Series(res, dates)
+            return pd.Series(res) if profile else pd.Series(res, dates)
