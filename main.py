3c9b27ff4c09012db82d69c06481cb91cb18c3ff
Anton Sturluson <anton.sturluson@gmail.com>
2020-09-13 13:04:00 -0400

    issue#13: parallel crawling summary enabled, headless switch added, crawling summary and the rest splitted.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import os
 import sys
 import time
 from datetime import datetime
+from functools import partial
+from os import cpu_count, path
+from multiprocessing import Process
 
 import schedule
 import pandas as pd
@@ -14,7 +16,7 @@ from utils import tools, DATADIR, PROFILEPATH, PROFILEBACKPATH
 def create_profile():
     """Merge .csv files in DATADIR and save to PROFILEPATH."""
     cols = ["Symbol", "Name", "IPOyear", "Sector", "Industry"]
-    dfs = [(pd.read_csv(os.path.join(DATADIR, path_))[cols]
+    dfs = [(pd.read_csv(path.join(DATADIR, path_))[cols]
                 .assign(Exchange=path_.split(".")[0].upper()))
            for path_ in ["amex.csv", "nasdaq.csv", "nyse.csv"]]
     df = pd.concat(dfs, axis=0)
@@ -26,7 +28,7 @@ def create_profile():
     df["Symbol"] = df["Symbol"].apply(process_symbol)
     df = df.loc[df["Symbol"].apply(lambda x: "~" not in x), :]
 
-    if os.path.exists(PROFILEPATH): # backup
+    if path.exists(PROFILEPATH): # backup
         tools.mv(PROFILEPATH, PROFILEBACKPATH)
     df.sort_values("Symbol").to_csv(PROFILEPATH, index=False) # save
 
@@ -38,6 +40,7 @@ override_profile = False
 preprocess_only = False
 force_summary = False
 k = None
+headless = True
 for sarg in sys.argv[1:]:
     if sarg.startswith("--debug"):
         debug = True
@@ -61,6 +64,9 @@ for sarg in sys.argv[1:]:
     if sarg.startswith("--k="): # for testing real data
         k = int(sarg.split("=")[1])
 
+    if sarg.startswith("--no-headless"):
+        headless = False
+
 
 def try_crawl(crawl_fn, symbol, fn_name):
     try:
@@ -69,18 +75,13 @@ def try_crawl(crawl_fn, symbol, fn_name):
         tools.log(f"[{symbol}] Failure crawling {fn_name}: {e}", debug)
 
 
-def main(summary):
-    symbols = tools.get_symbols(stock_only=False if init else True)
-    if debug or k:
-        symbols = symbols[:k if k else 5]
-
+def download_historical_data(symbols):
     if not preprocess_only:
         # Initialize driver
-        driver = ChromeDriver(init, debug)
+        driver = ChromeDriver(init, debug, headless)
 
-        # Main loop
+        # Main loop for initial crawling
         for i, symbol in enumerate(symbols):
-            if init or debug:
                 if not driver.exist(symbol):
                     continue
 
@@ -93,11 +94,6 @@ def main(summary):
                     try_crawl(driver.crawl_statistics, symbol, "statistics")
 
                 driver.reset_last_symbol_info()
-
-            if summary or debug:
-                # crawl daily Summary + Statistics section
-                #try_crawl(driver.crawl_summary, symbol, "summary")
-                driver.crawl_summary(symbol)
 
         # quit driver
         driver.quit()
@@ -112,8 +108,24 @@ def main(summary):
             profiledf["Currency"] = driver.currencys
             profiledf.to_csv(PROFILEPATH, index=False)
 
-    if init or debug:
-        init_process(symbols, init, debug)
+    init_process(symbols, init, debug)
+
+
+def crawl_summary_helper(symbols):
+    driver = ChromeDriver(init, debug, headless)
+    driver.crawl_summary(symbols)
+    driver.quit()
+
+
+def crawl_summary(symbols):
+    if __name__ == "__main__":
+        jobs = []
+        for i in range(0, n := len(symbols), k := n // cpu_count()):
+            jobs.append(Process(target=crawl_summary_helper,
+                                args=(symbols[i : min(i + k, n)],)))
+            jobs[-1].start()
+        while jobs:
+            jobs.pop().join()
 
 
 if __name__ == "__main__":
@@ -123,23 +135,27 @@ if __name__ == "__main__":
     tools.log(f"Starting crawler ({debug=}) - {today:%Y-%m-%d:%H-%M-%S}", debug)
     tools.log("=" * 42, debug)
 
-    if not os.path.exists(PROFILEPATH) or override_profile:
+    symbols = tools.get_symbols(stock_only=False if init else True)
+    if k or debug:
+        symbols = symbols[len(symbols)-k:len(symbols)]
+
+    if not path.exists(PROFILEPATH) or override_profile:
         create_profile()
         init = True
 
-    if init or debug or preprocess_only:
-        main(False)
+    if init or preprocess_only:
+        download_historical_data(symbols)
         init = False
 
     if force_summary:
-        main(True)
+        crawl_summary(symbols)
 
     if not debug and schedule_crawler:
-        schedule.every().monday.at("19:00").do(main, True)
-        schedule.every().tuesday.at("19:00").do(main, True)
-        schedule.every().wednesday.at("19:00").do(main, True)
-        schedule.every().thursday.at("19:00").do(main, True)
-        schedule.every().friday.at("19:00").do(main, True)
+        schedule.every().monday.at("19:00").do(crawl_summary, symbols)
+        schedule.every().tuesday.at("19:00").do(crawl_summary, symbols)
+        schedule.every().wednesday.at("19:00").do(crawl_summary, symbols)
+        schedule.every().thursday.at("19:00").do(crawl_summary, symbols)
+        schedule.every().friday.at("19:00").do(crawl_summary, symbols)
 
         i = 0
         while True:
